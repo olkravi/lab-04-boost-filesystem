3d987af462b103d729d9002d57a64a7b0eacc15b
kirill050 <kirilltikhonov050@gmail.com>
2019-12-19 16:33:07 +0300

    19.12.19 16:33

--- tests/test.cpp
+++ tests/test.cpp
@@ -4,9 +4,16 @@
 #include <header.hpp>
 
 TEST(Example, EmptyTest) {
-    auto path_ftp = boost::filesystem::path{"./../misc/ftp"};
+    auto curr_path = boost::filesystem::current_path();
+    string str_curr_path = curr_path.string();
+    if (str_curr_path.find("sources") != string::npos)
+    {
+        str_curr_path.erase(str_curr_path.rfind("/"),
+                        str_curr_path.length() - 1);
+        str_curr_path += string("/misc/ftp");
+    }
+    auto path_ftp = boost::filesystem::path{str_curr_path.c_str()};
     my_FTP result(path_ftp);
-    //result.result();
     EXPECT_TRUE(true);
 }
 
